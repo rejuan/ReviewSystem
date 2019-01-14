ce80abe0ac2194849e025693da6c9fbb89a1a390
A M S Rejuan <rejuan.shuvro@gmail.com>
2019-01-14 19:12:10 +0600

    company model without validation

---
+++ models/company.js
@@ -0,0 +1,41 @@
+const Joi = require("joi");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+const companySchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 255
+    },
+    image: String,
+    contact: {
+        mobile: String,
+        address: String,
+        website: String
+    },
+    details: String,
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+});
+
+const Company = mongoose.model("Company", companySchema);
+
+function addValidate(company) {
+    const schema = {
+        name: Joi.string()
+            .min(3)
+            .max(255)
+            .required()
+    };
+
+    return Joi.validate(company, schema);
+}
+
+exports.Company = Company;
+exports.addValidate = addValidate;
