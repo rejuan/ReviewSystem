f55bd4cb6abca78854c78f15b204d065cda8d243
A M S Rejuan <rejuan.shuvro@gmail.com>
2019-02-02 18:13:57 +0600

    Edit response done

--- routes/response.js
+++ routes/response.js
@@ -20,4 +20,19 @@ router.post("/", auth, async (req, res) => {
     res.send(_.pick(review, ["_id", "user", "company", "star", "title", "details", "response"]));
 });
 
+router.put("/", auth, async (req, res) => {
+    const {error} = responseValidate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    let review = await Review.findById(req.body.id).populate('company');
+    if(!review) return res.status(404).send("Not found");
+
+    if(review.company.user.toString() != req.user._id) return res.status(401).send("You are not the company owner.");
+
+    review.response = req.body.response;
+    review = await review.save();
+
+    res.send(_.pick(review, ["_id", "user", "company", "star", "title", "details", "response"]));
+});
+
 module.exports = router;
--- tests/response.test.js
+++ tests/response.test.js
@@ -128,6 +128,79 @@ describe("/api/response", () => {
 
     });
 
+    describe("PUT /", () => {
+
+        const exec = (requestObjet, token) => {
+            return request(server)
+                .put(url)
+                .set('x-auth-token', token)
+                .send(requestObjet);
+        };
+
+        it("should return 401 if no JWT", async () => {
+            const token = "";
+            const res = await exec(responseData, token);
+            expect(res.status).toBe(401);
+        });
+
+        it("should return 400 if JWT not valid", async () => {
+            const token = "1234";
+            const res = await exec(reviewData, token);
+            expect(res.status).toBe(400);
+        });
+
+        it("should return 404 if no review found", async () => {
+            const token = user.generateAuthToken();
+            responseData.id = user._id; // wrong id
+            const res = await exec(responseData, token);
+            expect(res.status).toBe(404);
+        });
+
+        it("should return 401 if not company owner", async () => {
+            const testUser = await saveUser(name, "response1@test.com", password);
+            const token = testUser.generateAuthToken();
+            const res = await exec(responseData, token);
+            expect(res.status).toBe(401);
+        });
+
+        it("should return 400 if no response", async () => {
+            const token = user.generateAuthToken();
+            delete responseData.response;
+            const res = await exec(responseData, token);
+            expect(res.status).toBe(400);
+        });
+
+        it("should return 400 if no review id", async () => {
+            const token = user.generateAuthToken();
+            delete responseData.id;
+            const res = await exec(responseData, token);
+            expect(res.status).toBe(400);
+        });
+
+        it("should return 400 if response less than 3", async () => {
+            const token = user.generateAuthToken();
+            responseData.response = "a";
+            const res = await exec(responseData, token);
+            expect(res.status).toBe(400);
+        });
+
+        it("should return 400 if response more than 512", async () => {
+            const token = user.generateAuthToken();
+            responseData.response = new Array(514).join("a");
+            const res = await exec(responseData, token);
+            expect(res.status).toBe(400);
+        });
+
+        it("should return 200 if valid input", async () => {
+            review.response = "test";
+            review = await review.save();
+            const token = user.generateAuthToken();
+            const res = await exec(responseData, token);
+            expect(res.status).toBe(200);
+        });
+
+    });
+
 });
 
 async function saveUser(name, email, password) {
